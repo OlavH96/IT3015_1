dbece0de0e287472d8056783d381dd259e866db1
Faiter119 <olavh96@gmail.com>
2018-09-04 14:30:39 +0200

    Working nn!

--- src/KeithTesting.py
+++ src/KeithTesting.py
@@ -1,25 +1,20 @@
 import tensorflow as tf
 import numpy as np
 import tflowtools as tft
+import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
 
     # 3 input nodes
     # 1 output node
     # 2 hidden nodes i ett layer
-    #
-    #
-    #
-    #
-    #
-    #
 
-    input_size = 101
+    input_size = 5
+    hidden_nodes = 3
     output_size = 1
     data_length = 100
-    learning_rate = 0.001
-    steps = 100
-    hidden_nodes = 3
+    learning_rate = 0.5
+    steps = 5000
 
     raw_data = tft.gen_symvect_dataset(input_size, data_length)
 
@@ -28,37 +23,85 @@ if __name__ == '__main__':
     print(raw_data)
     print(data)
     print(labels)
+    labels = np.array(labels)
 
     x = tf.placeholder(dtype=tf.float32, shape=[None, input_size], name='x')
+    y = tf.placeholder(dtype=tf.float32, shape=[None, output_size], name='y')
 
-    w1 = tf.Variable(tf.random_normal([input_size, hidden_nodes], stddev=0.03), name='w1')
-    b1 = tf.Variable(tf.random_normal([hidden_nodes], stddev=0.03), name='b1')
+    w = tf.Variable(tf.random_normal([input_size, hidden_nodes], stddev=0.03), name='w1')
+    b = tf.Variable(tf.random_normal([hidden_nodes]), name='b1')
+
+    hidden_output = tf.tanh(tf.matmul(x, w) + b)
 
     w2 = tf.Variable(tf.random_normal([hidden_nodes, output_size], stddev=0.03), name='w2')
     b2 = tf.Variable(tf.random_normal([output_size]), name='b2')
 
-    hidden_out = tf.nn.relu(tf.add(tf.matmul(x, w1), b1))
+    y_est = tf.sigmoid(tf.matmul(hidden_output, w2) + b2)
 
-    y = tf.nn.sigmoid(tf.add(tf.matmul(hidden_out, w2), b2))
+    error = tf.reduce_mean(tf.square(y_est - y))  # MSE
 
-    labels = np.array(labels)
-    error = tf.reduce_mean(tf.square(labels - y))
     optimizer = tf.train.GradientDescentOptimizer(learning_rate)
     training = optimizer.minimize(error)
 
     sess = tf.Session()
     sess.run(tf.global_variables_initializer())
 
+    # tft.viewprep(sess)
+    # tft.fireup_tensorboard(logdir='probeview')
+
+    errors = []
     for i in range(steps):
-        _, acc = sess.run([training, y], feed_dict={x: data, y: labels})
-        if i % 10 == 9:
-            print("Accuracy")
-            print(acc)
+        _, err = sess.run([training, error], feed_dict={x: data, y: labels})
+        errors.append(err)
+        # if i % 10 == 9:
+        # print(acc)
+    plt.plot(errors)
+    plt.xlabel("Iterations")
+    plt.ylabel("Error")
+    plt.show()
 
-    raw_test_data = tft.gen_symvect_dataset(input_size, 100)
+    test_data_size = 1000
 
+    raw_test_data = tft.gen_symvect_dataset(input_size, test_data_size)
+    print("Test Data")
+    print(raw_test_data)
     test_data = [data[:input_size] for data in raw_test_data]
     test_labels = [[data[input_size]] for data in raw_test_data]
+    print(test_data)
 
-    res = sess.run(y, feed_dict={x: test_data})
+    correct = 0
+    for i in range(test_data_size):
+        data = test_data[i]
+        label = test_labels[i][0]
+
+        est = sess.run(y_est, feed_dict={x: [data]})[0]
+        print(label)
+        print(est)
+        if label == 1 and est > 0.5:
+            correct += 1
+        if label == 0 and est < 0.5:
+            correct += 1
+
+    print(correct, " / ", test_data_size)
+
+    exit(1)
+    res = sess.run(y_est, feed_dict={x: test_data})
+    print("Result")
     print(res)
+
+    correct = 0
+    for i in range(test_data_size):
+        data = test_data[i]
+        label = test_labels[i][0]
+        est = res[i][0]
+
+        print(data)
+        print(label)
+        print(est)
+
+        if label == 1 and est > 0.5:
+            correct += 1
+        if label == 0 and est < 0.5:
+            correct += 1
+
+    print(correct)
